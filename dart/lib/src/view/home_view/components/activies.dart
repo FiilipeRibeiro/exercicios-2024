fdc38beb09975f84348574d8fb9120098cad6765
FiilipeRibeiro <Filipecaldas3999@gmail.com>
2024-05-16 17:15:54 -0300

    feat: add Activities

---
+++ dart/lib/src/view/home_view/components/activies.dart
@@ -0,0 +1,111 @@
+import 'package:chuva_dart/src/model/chuva_model.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Activities extends StatelessWidget {
+  final List<ChuvaModel> chuvaList;
+
+  const Activities({super.key, required this.chuvaList});
+
+  @override
+  Widget build(BuildContext context) {
+    int hexColorToInt(String? hexColor) {
+      if (hexColor == null || hexColor.isEmpty) {
+        return 0xFF000000;
+      }
+      hexColor = hexColor.toUpperCase().replaceAll('#', '');
+      if (hexColor.length == 6) {
+        hexColor = 'FF$hexColor';
+      }
+      return int.parse(hexColor, radix: 16);
+    }
+
+    return ListView.separated(
+      separatorBuilder: (context, index) => Container(
+        height: 2,
+      ),
+      itemCount: chuvaList.length,
+      itemBuilder: (_, int index) {
+        final chuva = chuvaList[index];
+        final name = chuva.people.map((person) => person.name).join(', ');
+        String extractTime(String dateTimeStr) {
+          DateTime dateTime = DateTime.parse(dateTimeStr);
+          return DateFormat('HH:mm').format(dateTime);
+        }
+
+        return Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 120,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(3, 3),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 8,
+                      height: 120,
+                      decoration: BoxDecoration(
+                        color: Color(hexColorToInt(chuva.color)),
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          bottomLeft: Radius.circular(10),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: ListTile(
+                        title: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '${chuva.type} de ${extractTime(chuva.start)} até ${extractTime(chuva.end)}',
+                              style: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            ListTile(
+                              contentPadding: EdgeInsets.zero,
+                              title: Text(
+                                chuva.title,
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              subtitle: Text(
+                                name,
+                                style: const TextStyle(
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
--- dart/lib/src/view/home_view/home_view.dart
+++ dart/lib/src/view/home_view/home_view.dart
@@ -1,22 +1,143 @@
 import 'package:flutter/material.dart';
-import '../activies_view/activies.dart';
+import 'package:intl/intl.dart';
+
+import '../../model/chuva_model.dart';
+import '../../repository/chuva_service.dart';
+import 'components/activies.dart';
 import 'components/buttom_app_bar.dart';
 import '../widgets/header_app_bar.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final DateTime _currentDate = DateTime(2023, 11, 26);
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: PreferredSize(
+    String formattedMonth = DateFormat('MMM').format(_currentDate);
+    return Scaffold(
+      appBar: const PreferredSize(
         preferredSize: Size.fromHeight(150),
         child: HeaderAppBar(
           subtitle: '\nProgramação',
           buttonAppBar: ButtomAppBar(),
         ),
       ),
-      body: Activies(),
+      body: FutureBuilder<List<ChuvaModel>>(
+        future: ChuvaService().getChuva(),
+        builder:
+            (BuildContext context, AsyncSnapshot<List<ChuvaModel>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (snapshot.hasData) {
+            List<ChuvaModel> chuvaList = snapshot.data!;
+            return DefaultTabController(
+              length: 5,
+              child: Column(
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 6, right: 6),
+                        child: Column(
+                          children: [
+                            Text(
+                              formattedMonth,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 22,
+                              ),
+                            ),
+                            Text(
+                              '${_currentDate.year}',
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 1),
+                          child: Container(
+                            height: 55,
+                            decoration: const BoxDecoration(
+                              color: Colors.blue,
+                            ),
+                            child: TabBar(
+                              tabAlignment: TabAlignment.start,
+                              indicator: const BoxDecoration(),
+                              isScrollable: true,
+                              labelColor: Colors.white,
+                              unselectedLabelColor: Colors.grey,
+                              tabs: [
+                                for (var day = 26; day <= 30; day++)
+                                  Tab(
+                                    text: day.toString(),
+                                  ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Expanded(
+                    child: TabBarView(
+                      children: [
+                        Activities(
+                          chuvaList: chuvaList
+                              .where((chuva) =>
+                                  DateTime.parse(chuva.start).day == 26)
+                              .toList(),
+                        ),
+                        Activities(
+                          chuvaList: chuvaList
+                              .where((chuva) =>
+                                  DateTime.parse(chuva.start).day == 27)
+                              .toList(),
+                        ),
+                        Activities(
+                          chuvaList: chuvaList
+                              .where((chuva) =>
+                                  DateTime.parse(chuva.start).day == 28)
+                              .toList(),
+                        ),
+                        Activities(
+                          chuvaList: chuvaList
+                              .where((chuva) =>
+                                  DateTime.parse(chuva.start).day == 29)
+                              .toList(),
+                        ),
+                        Activities(
+                          chuvaList: chuvaList
+                              .where((chuva) =>
+                                  DateTime.parse(chuva.start).day == 30)
+                              .toList(),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return const Center(
+              child: Text('No data available'),
+            );
+          }
+        },
+      ),
     );
   }
 }
